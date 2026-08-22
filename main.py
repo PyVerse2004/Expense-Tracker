9d44cb6f8c8fc5a62852a88ba9814dad3516eb9d
Sina <www.mahonia@gmail.com>
2026-08-23 00:44:57 +0330

    Change init Function And add Function and Rewrite total function

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from datetime import datetime
 
 class ExpenseTracker:
-    def __init__(self , amount , category , description):
+    def __init__(self):
+        self.expenses_list = []
         self.t_id = 1
+
+    def add_expense(self, amount , category , description):
         self.amount = amount
         self.category = category
         self.description = description
         self.date = datetime.now()
-        self.expenses_list = []
-
-    def add_expense(self):
+        
         track = {
             "id" : self.t_id , 
             "amount" : self.amount , 
@@ -17,13 +18,21 @@ class ExpenseTracker:
             "description" : self.description , 
             "date" : self.date
         }
-        self.t_id +=1
+        
         self.expenses_list.append(track)
+        self.t_id +=1
 
     def delete_expense(self , item):
         self.expenses_list.pop(item-1)
         
 
+    def total_expense(self):
+        self.total = 0
+
+        for track in self.expenses_list:
+            self.total += track["amount"]
+
+        return self.total
 
 
 
@@ -33,22 +42,16 @@ class ExpenseTracker:
 
 
 
+# acc = ExpenseTracker()
+# acc1 = ExpenseTracker()
 
+# acc.add_expense(250 , "food" , "lunch")
+# acc.add_expense(120 , "taxi" , "Transport")
 
+# print(acc.expenses_list)
 
+# # acc.delete_expense(1)
 
+# print(acc.expenses_list)
 
-
-
-
-
-acc = ExpenseTracker(255 , "food" , "description")
-
-acc.add_expense()
-
-
-acc1 = ExpenseTracker(2 , "foodsdaad" , "dn")
-acc.add_expense()
-print(acc.expenses_list)
-acc.delete_expense(2)
-print(acc.expenses_list)
+# print(acc.total_expense())
